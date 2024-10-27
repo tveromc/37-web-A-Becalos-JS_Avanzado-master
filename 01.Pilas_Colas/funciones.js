b5998e7d53f898481410d935c7ab53a949a5c3a7
tveromc <you@example.com>
2024-10-27 01:44:46 -0600

    Ejemplo de pila de ejecución

---
+++ 01.Pilas_Colas/funciones.js
@@ -0,0 +1,23 @@
+function showNumberOne() {
+    console.log(1)
+}
+
+function showNumberTwo() {
+    setTimeout(() => {
+        console.log(2)
+    }, 7000)
+}
+
+function showNumberThree() { 
+    console.log(3)
+}   
+
+function showNumberFour(){
+    console.log(4)
+}
+
+// Ejecución de las funciones
+showNumberOne()
+showNumberTwo()
+showNumberThree()
+showNumberFour()
